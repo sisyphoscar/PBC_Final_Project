14b9dec1614abe04060cc0b91103945565e11560
Oscarxi <93855298+Oscarxi@users.noreply.github.com>
2022-06-04 23:36:39 +0800

    merge error fixed

--- refine.py
+++ refine.py
@@ -1,9 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import datetime
-import json
-# pip install lxml
 
 def check_req_url(url): 
     resp = requests.get(url) 
@@ -12,7 +9,6 @@ def check_req_url(url):
         return "fail" 
     else:
         return resp.text
-
 def get_week_new_movies(webpage): #抓取電影資訊
     soup = BeautifulSoup(webpage, 'html.parser') #網頁解析
     movies = [] #域設電影資訊存這裡 
@@ -99,95 +95,6 @@ price = [290,320,400,280,280,290,290,340,330,270,290,290,340,370,310,370,300,290
          ]
 
 
-# Yahoo 電影分類
-
-# 獲得 HTML
-def getHtml(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "lxml")
-    return soup
-
-
-def getMoviepages(soup):
-    # 單個頁面連接到的所有電影資訊頁面
-    url_block = soup.find_all("div", "release_movie_name")
-    result = []
-    for movie in url_block:
-        result.append(movie.find("a", "gabtn").get("href"))
-    return result
-
-
-# 得到資訊list
-def getMovieTags(url_set):
-    result = []
-    for i in range(len(url_set)):
-        url = url_set[i]
-        movie = getHtml(url)
-        movieTags = movie.find_all("div", "level_name")
-        name = movie.find("h1").get_text(strip = True)
-        for tag in range(len(movieTags)):
-            movieTags[tag] = movieTags[tag].get_text(strip = True)
-        movieTags.remove("期待度")
-        movieTags.remove("滿意度")
-        result.append([name, movieTags])
-    return result
-
-#Output
-
-# 票房
-# 計算日期
-def getday(y,m,d, delta):
-    today = datetime.datetime(y,m,d)
-    result_date = today + datetime.timedelta(days = delta)
-    return result_date
-
-def getBoxOffice():
-    today = datetime.date.today()
-    start = getday(today.year, today.month, today.day, -(today.weekday() + 14))
-    end = getday(start.year, start.month, start.day, 6)
-
-    # 轉成 URL 所需的 string
-    start = f"{start.year}/{start.month}/{start.day}"
-    end = f"{end.year}/{end.month}/{end.day}"
-
-    # 進入json檔網址
-    url = f"https://boxoffice.tfi.org.tw/api/export?start={start}&end={end}"
-    response = requests.get(url)
-
-    # 轉成json
-    data = json.loads(response.text)
-
-    # 轉成 DataFrame
-    movie_info = data["list"]
-    column = ["電影名稱", "票房", "金額"]
-    boxOffice = []
-    for movie in movie_info:
-        boxOffice.append([movie["name"], movie["totalTickets"], movie["totalAmounts"]])
-
-    # Output
-    result = pd.DataFrame(boxOffice, columns = column)
-    return result
-
-
-def getMovieClass():
-    result = pd.DataFrame()
-    page = 1
-    while True:
-        url = f"https://movies.yahoo.com.tw/movie_intheaters.html?page={page}"
-        soup = getHtml(url)
-        url_set = getMoviepages(soup)
-        movie_tag = getMovieTags(url_set)
-        single_result = pd.DataFrame(movie_tag, columns = ["電影名稱","分類"])
-        result = pd.concat([result, single_result], axis = 0).reset_index(drop = True)
-        if page == 4:
-            print('66%loading')
-        if soup.find("li", "nexttxt disabled") == None:
-            page += 1
-        else:
-            break
-        print(f"page:{page}")
-    return result
-
 def getTimeInfo():
     fin = pd.DataFrame()
     for i in range(len(yahoo_movie_url)):
@@ -200,23 +107,9 @@ def getTimeInfo():
         movies['票價'] = price[i]
         fin = pd.concat([fin,movies],axis=0)
     fin = fin.explode('電影場次')
-    # fin.to_csv("電影總覽.csv")
+    fin = fin.reset_index(drop=True)
+    fin.to_csv("電影總覽.csv")
+    print('finish')
     return fin
 
-if __name__ == "__main__":
-    try:
-        print('loading...')
-
-        table1 = getTimeInfo()
-        print('33%loading...')
-
-        table2 = getMovieClass()
-        result = table1.merge(table2, on = "電影名稱", how = 'left')
-
-        table3 = getBoxOffice()
-        result = result.merge(table3, on = "電影名稱", how = 'left')
-        # table3.to_csv("電影票房.csv")
-        result.to_csv("電影資訊.csv")
-        # print("finished")
-    except Exception as e:
-        print(e)
+print(getTimeInfo())
